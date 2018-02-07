c802aa9b1d26d7fbb034eed971e2ecf284e9ed51
Rodrigo da Silva Presser <rodrigopresser@gmail.com>
2018-02-07 01:38:02 -0200

    correção de string.

--- app/src/main/java/br/com/fly01belezaestetica/MainActivity.kt
+++ app/src/main/java/br/com/fly01belezaestetica/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView1.setText("Hellor World")
+        textView1.setText("Hello World")
     }
 }
