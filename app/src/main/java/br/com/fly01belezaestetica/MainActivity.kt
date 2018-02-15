6ffb6d19145ba4fc5335aac02288ff2245a7d452
Rodrigo Presser <rodrigopresser@gmail.com>
2018-02-14 23:06:23 -0200

    inclusão de layout para tela de login

--- app/src/main/java/br/com/fly01belezaestetica/MainActivity.kt
+++ app/src/main/java/br/com/fly01belezaestetica/MainActivity.kt
@@ -1,16 +1,50 @@
 package br.com.fly01belezaestetica
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
+    }
 
-        textView1.setText("Hello World")
+    class MainActivityUI : AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            verticalLayout {
+                gravity = Gravity.CENTER
+                padding = dip(20)
+
+                textView {
+                    gravity = Gravity.CENTER
+                    text = "Login"
+                    textColor = Color.BLACK
+                    textSize = 24f
+                }.lparams(width = matchParent) {
+                    margin = dip(20)
+                }
+
+                val name = editText {
+                    hint = "Nome"
+                }
+
+                editText {
+                    hint = "Senha"
+                    lines = 3
+                }
+
+                button("Entrar") {
+                    onClick {
+                        toast( "Hey ${name.text}! Thank you for contacting us. We will get in touch with you soon.")
+                    }
+                }.lparams(dip(280), sp(80))
+
+            }
+        }
     }
 }
