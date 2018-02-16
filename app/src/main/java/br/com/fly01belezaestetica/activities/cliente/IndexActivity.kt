d24ff20e60270e6a397d5b906e773fa06e314238
Rodrigo Presser <rodrigopresser@gmail.com>
2018-02-16 01:42:46 -0200

    inclusão de nova activity

---
+++ app/src/main/java/br/com/fly01belezaestetica/activities/cliente/IndexActivity.kt
@@ -0,0 +1,11 @@
+package br.com.fly01belezaestetica.activities.cliente
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
