a2e57ef9665e5cc82119b75d7d5d8457664c79c6
Rodrigo Presser <rodrigopresser@gmail.com>
2018-02-16 23:51:48 -0200

    remoção da barra de status

--- app/src/main/java/br/com/fly01belezaestetica/MainActivity.kt
+++ app/src/main/java/br/com/fly01belezaestetica/MainActivity.kt
@@ -3,10 +3,9 @@ package br.com.fly01belezaestetica
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
 import android.view.Gravity
-import android.view.View
 import br.com.fly01belezaestetica.activities.cliente.IndexActivity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
@@ -17,63 +16,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         MainActivityUI().setContentView(this)
     }
-}
 
-class MainActivityUI : AnkoComponent<MainActivity> {
-    fun proximaTela(v: View)
-    {
-        /*val intent = Intent(v.context, MainActivity::class.java)
-        startActivity(intent)
+    class MainActivityUI : AnkoComponent<MainActivity> {
 
-        val intent = Intent(this, DetalhesContatoActivity::class.java)
-        intent.putExtra("contato", c)
-        startActivity(intent)*/
-
-        val intent = Intent(v.context, MainActivity::class.java)
-        startActivity(intent)
-    }
-
-    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
-        verticalLayout {
-            gravity = Gravity.CENTER
-            padding = dip(20)
-            setBackgroundResource(R.drawable.background_01)
-
-            imageView(R.drawable.beleza_estetica).lparams(width = matchParent) {
-                padding = dip(20)
-                margin = dip(15)
-            }
-
-            /*textView {
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            verticalLayout {
                 gravity = Gravity.CENTER
-                textColor = Color.WHITE
-                textSize = 24f
-                textResource = R.string.login_text
-            }.lparams(width = matchParent) {
-                margin = dip(20)
-            }*/
+                padding = dip(20)
+                setBackgroundResource(R.drawable.background_01)
 
-            val name = editText {
-                hintResource = R.string.login_name_hint
-                hintTextColor = Color.WHITE
-                textColor = Color.WHITE
-            }
-
-            val password = editText {
-                hintResource = R.string.login_password_hint
-                hintTextColor = Color.WHITE
-                textColor = Color.WHITE
-            }
-
-            button(R.string.login_button) {
-                backgroundColor = Color.LTGRAY
-                onClick {
-                    startActivity(Intent(this@MainActivity, IndexActivity::class.java))
-                    proximaTela(MainActivity, )
-                    toast("Hey ${name.text}! Thank you for contacting us. We will get in touch with you soon.")
+                imageView(R.drawable.beleza_estetica).lparams(width = matchParent) {
+                    padding = dip(20)
+                    margin = dip(15)
                 }
 
-            }.lparams(dip(280), sp(50))
+                val name = editText {
+                    hintResource = R.string.login_name_hint
+                    hintTextColor = Color.WHITE
+                    textColor = Color.WHITE
+                }
+
+                val password = editText {
+                    hintResource = R.string.login_password_hint
+                    hintTextColor = Color.WHITE
+                    textColor = Color.WHITE
+                    inputType = InputType.TYPE_NUMBER_VARIATION_PASSWORD
+                }
+
+                button(R.string.login_button) {
+                    backgroundColor = Color.LTGRAY
+                    onClick {
+                        ui.ctx.startActivity(Intent(ui.ctx, IndexActivity::class.java))
+                        toast("Hey ${name.text}! Thank you for contacting us. We will get in touch with you soon.")
+                    }
+
+                }.lparams(dip(280), sp(50))
+            }
         }
     }
 }
